b217e536f3866cf45fe30d5098e26c9f33d58327
Björn Hillebrand <bjoern_hillebrand@hotmail.de>
2020-02-09 16:19:48 +0100

    Introduced a new interface IRvcComposer

---
+++ src/RiscVSim.Environment/IRvcComposer.cs
@@ -0,0 +1,14 @@
+﻿using RiscVSim.Environment.Decoder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiscVSim.Environment
+{
+    public interface IRvcComposer
+    {
+        Instruction ComposeInstruction(RvcPayload payload);
+
+        InstructionPayload ComposePayload(Instruction instruction, RvcPayload payload);
+    }
+}
---
+++ src/RiscVSim.Environment/Rv32I/RvcComposer.cs
@@ -0,0 +1,32 @@
+﻿using RiscVSim.Environment.Decoder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiscVSim.Environment.Rv32I
+{
+    public class RvcComposer : IRvcComposer
+    {
+        public RvcComposer()
+        {
+
+        }
+        public Instruction ComposeInstruction(RvcPayload payload)
+        {
+            //Instruction ins = new Instruction()
+
+            //return ins;
+
+            return null;
+        }
+
+        public InstructionPayload ComposePayload(Instruction ins, RvcPayload payload)
+        {
+            //InstructionPayload p = new InstructionPayload(ins, null);
+
+            //return p;
+
+            return null;
+        }
+    }
+}
